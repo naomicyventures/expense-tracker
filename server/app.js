edff41498f8a1eff8dd08634a898cec1af01dd7e
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-01 21:21:09 +0000

    server: setup routes

--- server/app.js
+++ server/app.js
@@ -2,23 +2,48 @@ import dotenv from 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './db/db.js';
-
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
-
-// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors);
+app.use(cors());
 
-const PORT = process.env.PORT;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const server = () => {
-  connectDB();
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}
+const loadRoutes = async () => {
+  try {
+    const routeFiles = await readdir(`${__dirname}/routes`);
+    const routeModules = routeFiles
+      .filter(route => route.endsWith('.js'))
+      .map(async route => {
+        const { default: routeModule } = await import(`./routes/${route}`);
+        return routeModule;
+      });
 
+    const resolvedRouteModules = await Promise.all(routeModules);
 
-server();
+    resolvedRouteModules.forEach(routeModule => {
+      app.use('/api/v1', routeModule);
+    });
+  } catch (error) {
+    console.error('Error loading routes:', error);
+  }
+};
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    await loadRoutes();
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Server startup error:', error);
+  }
+};
+
+startServer();
