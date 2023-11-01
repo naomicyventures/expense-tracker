993e4b7be07ac60784b045bc3b20fd5f4e7ccbf3
Kwaku Boateng <kwakuboateng.dev@gmail.com>
2023-11-01 17:47:15 +0000

    server: mongodb connection established

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import { connectDB } from './db/db.js';
+
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors);
 
 const PORT = process.env.PORT;
 
 const server = () => {
+  connectDB();
   app.listen(PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
   });
